112a5b83a59e220267661ec8418c66e9abc9785b
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-10-31 09:59:38 +0700

    menambahkan divider and thickness again

--- app/src/main/java/com/example/input/TeksEdit.kt
+++ app/src/main/java/com/example/input/TeksEdit.kt
@@ -92,5 +92,13 @@ Button(
         alamat=textAlamat
     }
 )   {
-
+    Text(stringResource(R.string.submit))
 }
+
+Divider(
+    modifier = Modifier.padding(bottom = dimensionresource(R.dimen.padding_medium), top = dimensionResource(
+        id = R.dimen.padding_medium
+    )),
+    thickness = dimensionResource(R.dimen.divider_tipis),
+    color = Color.DarkGray
+)
